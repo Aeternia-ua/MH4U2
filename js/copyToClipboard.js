558fc39e2d378c65d304ad8a7d379df37621c1c0
Aeternia-ua <gherasymuk@gmail.com>
2020-05-20 10:58:42 +0300

    Browser detection

--- js/copyToClipboard.js
+++ js/copyToClipboard.js
@@ -1,12 +1,11 @@
-//Path to index.html file
-const lastSlashIndex = window.location.pathname.lastIndexOf("/");
-const origin = localStorage["mh4u-origin"]?localStorage["mh4u-origin"]:"/index.html";
-const newPath = window.location.pathname.substring(0, lastSlashIndex) + origin;
-const indexURL = window.location.origin + newPath;
+var lastSlashIndex = document.location.pathname.lastIndexOf("/");
+var origin = localStorage["mh4u-origin"]?localStorage["mh4u-origin"]:"/index.html";
+var newPath = window.location.pathname.substring(0, lastSlashIndex) + origin;
+var indexURL = window.location.origin + newPath;
 
 $(document).ready(function(){
 
-    let url = document.getElementById("urlTextField").value = indexURL;
+    var url = document.getElementById("urlTextField").value = indexURL;
 
     $(document).on('click', '#copy-btn', function() {
         copyText(url, this);  
@@ -14,18 +13,10 @@ $(document).ready(function(){
 
 });
 
-function copyText(text, context) {
+function copyText(text) {
     var textField = document.getElementById("urlTextField");
     textField.innerText = text;
-
-    if (context) {
-        context.parentNode.insertBefore(textField, context);
-    } else {
-        document.body.appendChild(textField);
-    }
-
     textField.select();
     document.execCommand('copy');
-    // textField.parentNode.removeChild(textField);
-    console.log('should have copied ' + text);
 }
+
